ff39de25e05373395dadf64368fb115649c7b73f
Jonathan Truong <jjtruong90@gmail.com>
2020-03-18 12:04:30 -0700

    Updates

---
+++ precache-manifest.87cd07ef34d3dc16406dd4df62568149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d857586353faaf20f9abd43ada69be82",
+    "url": "/google-books/index.html"
+  },
+  {
+    "revision": "0c070c6b90c84dc34bf8",
+    "url": "/google-books/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "09b931b7c0632fd474cf",
+    "url": "/google-books/static/js/2.60b5ba38.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/google-books/static/js/2.60b5ba38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c070c6b90c84dc34bf8",
+    "url": "/google-books/static/js/main.26116bd6.chunk.js"
+  },
+  {
+    "revision": "52c6f95ad72c0d107b08",
+    "url": "/google-books/static/js/runtime-main.93b1d62a.js"
+  }
+]);
